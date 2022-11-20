96db56745a6ba35be99676cccf7501fa9490f7a0
BernardoSoaresPT <114663133+BernardoSoaresPT@users.noreply.github.com>
2022-11-20 18:16:32 +0000

    Add files via upload

---
+++ JavaScriptExercise/script.js
@@ -0,0 +1,12 @@
+var name = "Bernardo";
+var age = 19;
+console.log(name)
+console.log(age)
+var object ={
+    name: "Bernardo",
+    age: 19,
+    isStudendt: true
+};
+var ageMultiplyed = object.age*7;
+var friend =["Tales", "Vasco" ,"Leonardo"];
+var caracteristicas =["Crackhead", "Artist" ,"Novo careca"];
